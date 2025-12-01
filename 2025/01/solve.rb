e4426b22b2b0d91fd733b7e6e47db9599c42c858
cankoeks <can.koeksen@rub.de>
2025-12-01 23:07:43 +0100

    Add Day1

---
+++ 2025/01/solve.rb
@@ -0,0 +1,21 @@
+require_relative "../utils"
+
+dial = 50
+part1, part2 = 0, 0
+
+AdventOfCode::Utils.with_timed_run do
+  AdventOfCode::Utils.read_input do |line|
+    x = line.gsub("L", "-").gsub("R", "+").to_i
+    old_dial = dial
+    dial += x
+
+    part2 += 1 if x < 0 && dial <= 0 && old_dial > 0
+    part2 += (dial < 0 ? dial.abs : dial) / 100
+
+    dial %= 100
+    part1 += 1 if dial == 0
+  end
+end
+
+puts part1
+puts part2
